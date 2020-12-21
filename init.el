b8c2fcec0610fc9c0f84227021ad436584b26800
Tiberius <vi@ceramic3d.ru>
2020-12-21 14:06:58 +0500

    colors [re]

--- init.el
+++ init.el
@@ -242,6 +242,11 @@
 (set-face-attribute 'smerge-refined-added nil :background "gray5")
 (set-face-attribute 'flycheck-error nil :foreground "yellow1") ; brightyellow
 
+(require 'company)
+(set-face-attribute 'company-tooltip-annotation nil :foreground "sandy brown")
+(set-face-attribute 'company-tooltip-common nil :foreground "green")
+(set-face-attribute 'company-tooltip-common-selection nil :foreground "magenta")
+
 ;; set transparency
 (defun on-after-init () (unless (display-graphic-p (selected-frame)) (set-face-background 'default "black-bg" (selected-frame))))
 
@@ -293,10 +298,33 @@
     '((background-color . "black")
       (vertical-scroll-bars)
       (alpha . 85)))
+ '(evil-collection-mode-list
+   '(2048-game ag alchemist anaconda-mode apropos arc-mode auto-package-update bm bookmark
+               (buff-menu "buff-menu")
+               calc calendar cider cmake-mode comint company compile consult
+               (custom cus-edit)
+               cus-theme dashboard daemons deadgrep debbugs debug dictionary diff-mode dired dired-sidebar disk-usage doc-view docker ebib edbi edebug ediff eglot explain-pause-mode elfeed elisp-mode elisp-refs elisp-slime-nav emms epa ert eshell eval-sexp-fu evil-mc eww finder flycheck flymake free-keys geiser ggtags git-timemachine gnus go-mode grep guix hackernews helm help helpful hg-histedit hungry-delete ibuffer image image-dired image+ imenu imenu-list
+               (indent "indent")
+               indium info ivy js2-mode leetcode lispy log-edit log-view lsp-ui-imenu lua-mode kotlin-mode macrostep man magit-todos monky mu4e mu4e-conversation neotree newsticker notmuch nov
+               (occur replace)
+               omnisharp org-present zmusic osx-dictionary outline p4
+               (package-menu package)
+               pass
+               (pdf pdf-view)
+               popup proced
+               (process-menu simple)
+               prodigy profiler python quickrun racer racket-describe realgud reftex restclient rg rjsx-mode robe rtags ruby-mode sh-script simple slime sly speedbar tab-bar tablist tabulated-list tar-mode
+               (term term ansi-term multi-term)
+               tetris thread tide timer-list transmission trashed typescript-mode vc-annotate vc-dir vc-git vdiff view vlf vterm w3m wdired wgrep which-key woman xref youtube-dl
+               (ztree ztree-diff)
+               xwidget))
  '(evil-shift-width 2)
  '(evil-undo-system 'undo-tree)
+ '(helm-minibuffer-history-key "M-p")
  '(lsp-diagnostics-flycheck-default-level 'info)
  '(lsp-modeline-code-actions-segments '(count icon))
+ '(lsp-rust-all-features t)
+ '(lsp-rust-analyzer-cargo-watch-command "clippy")
  '(lsp-rust-clippy-preference "on")
  '(lsp-rust-wait-to-build 500.0)
  '(lsp-ui-sideline-diagnostic-max-lines 10)
@@ -307,23 +335,23 @@
      "< " mode-line-misc-info mode-line-client mode-line-remote mode-line-buffer-identification mode-line-position evil-mode-line-tag))
  '(opascal-indent-level 2)
  '(package-selected-packages
-   '(format-all sdcv telega cyberpunk-theme timerfunctions clean-buffers all-the-icons evil-nerd-commenter google-translate unicode-fonts company-statistics lua-mode company-glsl glsl-mode origami auto-complete rainbow-delimiters json-mode magit xclip molokai-theme ## treemacs-evil linum-relative color-theme-modern rustic flycheck-rust rust-mode flycheck evil))
+   '(scala-mode lsp-java kotlin-mode groovy-mode clojure-mode sdcv telega cyberpunk-theme timerfunctions clean-buffers all-the-icons evil-nerd-commenter google-translate unicode-fonts company-statistics lua-mode company-glsl glsl-mode origami auto-complete rainbow-delimiters json-mode magit xclip molokai-theme ## treemacs-evil linum-relative color-theme-modern rustic flycheck-rust rust-mode flycheck evil))
  '(realgud-safe-mode nil)
  '(tab-width 2)
  '(telega-mode-line-mode t)
- '(telega-server-libs-prefix "~/.emacs.d/telega"))
+ '(telega-server-libs-prefix "~/.emacs.d/telega")
+ '(treemacs-ignored-file-predicates '(treemacs-ignore)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-tooltip-annotation ((t (:inherit company-tooltip :foreground "sandy brown"))))
- '(company-tooltip-common ((t (:inherit company-tooltip :foreground "green"))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-common :foreground "magenta"))))
  '(font-lock-comment-delimiter-face ((t (:foreground "gainsboro" :slant italic))))
  '(font-lock-comment-face ((t (:foreground "gainsboro" :slant italic))))
  '(lsp-lsp-flycheck-warning-unnecessary-face ((t (:inherit nil :extend nil :stipple nil :foreground "orchid" :strike-through nil :underline nil :slant italic))) t)
  '(magit-diff-their-highlight ((t (:inherit magit-diff-added-highlight :foreground "cyan"))))
+ '(magit-head ((t (:foreground "cyan" :weight bold))))
+ '(magit-reflog-checkout ((t (:foreground "cyan"))))
  '(rainbow-delimiters-base-error-face ((t (:inherit rainbow-delimiters-base-face :background "red" :foreground "yellow1"))))
  '(rainbow-delimiters-depth-1-face ((t (:inherit outline-1 :foreground "red1"))))
  '(rainbow-delimiters-depth-4-face ((t (:inherit outline-4 :foreground "magenta1"))))
